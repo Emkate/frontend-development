e87396a0d88a306820d723d1ed58b36abdaba2b4
Emkate <tkaczykmichal94@gmail.com>
2018-03-24 10:20:31 +0100

    lab4

--- lab4/app-compiled.js
+++ lab4/app-compiled.js
@@ -1,10 +1,40 @@
 "use strict";
 
-var evenPredicate = function evenPredicate(v) {
-  return v % 2 === 0;
-};
-var evens = [1, 2, 3, 4, 5, 6].filter(evenPredicate);
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-evens.forEach(function (v) {
-  return console.log(v);
-});
+// var evenPredicate = v => v % 2 === 0;
+// var evens = [1,2,3,4,5,6].filter(evenPredicate);
+
+// evens.forEach(v => console.log(v));
+
+// var aShape = {
+//     x: 4,
+//     y: 3,
+//     move: delta => this.x += delta
+// };
+
+
+// var aShape = {
+//     numbers: [1,2,3,4,5,6],
+//     scale: 2,
+//     move: function(){
+//         this.numbers = this.numbers.map((el) => {
+//             return el * this.scale;
+//         });
+//     }
+// }
+
+// aShape.move();
+
+// aShape.numbers.forEach(el => {
+//     console.log(el);
+// });
+
+var Shape = function Shape(x, y) {
+    _classCallCheck(this, Shape);
+
+    this.x = x;
+    this.y = y;
+};
+
+;
--- lab4/app.js
+++ lab4/app.js
@@ -1,11 +1,35 @@
 
-var evenPredicate = v => v % 2 === 0;
-var evens = [1,2,3,4,5,6].filter(evenPredicate);
+// var evenPredicate = v => v % 2 === 0;
+// var evens = [1,2,3,4,5,6].filter(evenPredicate);
 
-evens.forEach(v => console.log(v));
+// evens.forEach(v => console.log(v));
 
-var aShape = {
-    x: 4,
-    y: 3,
-    move: delta => this.x += delta
+// var aShape = {
+//     x: 4,
+//     y: 3,
+//     move: delta => this.x += delta
+// };
+
+
+// var aShape = {
+//     numbers: [1,2,3,4,5,6],
+//     scale: 2,
+//     move: function(){
+//         this.numbers = this.numbers.map((el) => {
+//             return el * this.scale;
+//         });
+//     }
+// }
+
+// aShape.move();
+
+// aShape.numbers.forEach(el => {
+//     console.log(el);
+// });
+
+class Shape {
+    constructor (x, y) {
+        this.x = x;
+        this.y = y;
+    }
 };
